3e422124b93718e4973b3e52c16fd46adcf291aa
Lars Koester <lkoester2015@fau.edu>
2019-04-11 16:29:37 -0400

    testing

--- src/loginRegister/LoginView.java
+++ src/loginRegister/LoginView.java
@@ -1,6 +1,7 @@
 package loginRegister;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -18,11 +19,16 @@ import javafx.stage.Stage;
                 Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
                 Scene scene = new Scene(root);
                 
-                scene.getStylesheets().add(getClass().getResource("Login.css").toExternalForm());
+                //scene.getStylesheets().add(getClass().getResource("Login.css").toExternalForm());
             
                 primaryStage.setTitle("Login-ToolDepot"); 
                 primaryStage.setScene(scene);
                 primaryStage.show();
+                
+                primaryStage.focusedProperty().addListener((ov, onHidden, onShown) -> {
+                    if(!primaryStage.isFocused())
+                        Platform.runLater(() -> primaryStage.close());
+                });
             
             } catch(Exception e) {
                 e.printStackTrace();
